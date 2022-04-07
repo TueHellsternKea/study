295051178858ca14d0501e1357adf2c93ea29dfb
Tue Hellstern <tue@officegeek.dk>
2022-04-07 22:37:28 +0200

    div

--- 4sem/05-Distribuerede_systemer/code/DemoFlask.py
+++ 4sem/05-Distribuerede_systemer/code/DemoFlask.py
@@ -1,6 +1,6 @@
 # Demo Flask
 
-# Importing flask module in the project is mandatory
+# Importing
 from flask import Flask
 
 # Flask constructor takes the name of
@@ -11,13 +11,17 @@ app = Flask(__name__)
 # which tells the application which URL should call
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
 
 # main driver function
 if __name__ == '__main__':
-
-	# run() method of Flask class runs the application
-	# on the local development server.
 	app.run()
